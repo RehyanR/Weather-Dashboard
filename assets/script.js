1b1d7503f45fc7125e212a1096079f024a02efdc
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-27 13:42:00 +0000

    changed var to const for api key and api url

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // Function to fetch weather data from the API
   function fetchWeatherData(cityName) {
-    var apiKey = "5053ec4fe604b73e4ed031612d75c596";
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5053ec4fe604b73e4ed031612d75c596";
+    const apiKey = "5053ec4fe604b73e4ed031612d75c596";
+    const apiUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5053ec4fe604b73e4ed031612d75c596";
   }
 });
