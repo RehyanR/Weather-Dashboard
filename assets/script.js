3deb94b1e2496bf8ec60c29f18c624f45454b3e0
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-27 10:14:44 +0000

    added API key to js file

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // Function to fetch weather data from the API
+    function fetchWeatherData(cityName) {
+      var apiKey = '5053ec4fe604b73e4ed031612d75c596'; }} )
