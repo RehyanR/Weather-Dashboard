7f3ebc9cf8f681da95c58978253a70f7b0c4f1e2
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-28 11:02:46 +0000

    restarting JS

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  // Function to fetch weather data from the API
-  function fetchWeatherData(cityName) {
-    const apiKey = "5053ec4fe604b73e4ed031612d75c596";
-    const apiUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5053ec4fe604b73e4ed031612d75c596";
-  }
-});
