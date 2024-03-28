adbb8244c24dbb190b04dc6a8b8077b899180b18
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-28 11:40:11 +0000

    added function to fetch data from url displayinf weather data

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,9 @@ function getWeatherData(city) {
   var apiKey = "5053ec4fe604b73e4ed031612d75c596"; // Added my API Key from OpenWeatherMap
   var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 }
+
+$.get(apiUrl)
+      .done(function(data) {
+        displayWeather(data);
+        saveToLocalStorage(city);
+      })
